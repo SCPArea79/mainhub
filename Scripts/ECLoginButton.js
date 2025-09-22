23b87374b3af849233922352b5201c4c5cb29f33
Area79 <okwhynot53@gmail.com>
2025-09-22 13:03:05 -0400

    #

--- Scripts/ECLoginButton.js
+++ Scripts/ECLoginButton.js
@@ -1,21 +1,21 @@
 var wrongCounter = 0;
-    function LoginButton() {
-      var user = document.getElementById("user");
-      var pass = document.getElementById("pass");
-      var wrongMsg = document.getElementById("WrongMsg");
+  function LoginButton() {
+    var user = document.getElementById("user");
+    var pass = document.getElementById("pass");
+    var wrongMsg = document.getElementById("WrongMsg");
 
-      if (user.value == "1967EthicsCommittee" && pass.value != "") {
-        location.href =
-          "https://sites.google.com/view/the-ethics-committee-homepage/home";
-      } else if (user.value == "Secret" && pass.value == "Password") {
-        location.href =
-          "funnywebsite.html";
-      } else {
-        wrongCounter = wrongCounter + 1;
-        wrongMsg.style.visibility = "visible";
-      }
-
-      if (wrongCounter == 3) {
-        location.href = "accessdenied.html";
-      }
+    if (user.value == "1967EthicsCommittee" && pass.value != "") {
+      location.href =
+        "https://sites.google.com/view/the-ethics-committee-homepage/home";
+    } else if (user.value == "Secret" && pass.value == "Password") {
+      location.href =
+        "funnywebsite.html";
+    } else {
+      wrongCounter = wrongCounter + 1;
+      wrongMsg.style.visibility = "visible";
     }
+
+    if (wrongCounter == 3) {
+      location.href = "accessdenied.html";
+    }
+  }
